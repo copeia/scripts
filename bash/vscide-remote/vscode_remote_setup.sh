19613d89c8866391879ab311b85841fcb8ea66c4
Ian Copeland <copeiaj@mgmail.com>
2020-09-06 06:14:29 +0200

    More changes to vscode-remote script

--- bash/vscide-remote/vscode_remote_setup.sh
+++ bash/vscide-remote/vscode_remote_setup.sh
@@ -15,7 +15,18 @@
 # Install dep software 
 printf "\e[1;32m\nInstalling Dependency Software...\e[0m\n\n"
 dnf -y update 
-dnf -y install wget tar vim zsh util-linux-user git docker vault terraform
+dnf -y install wget tar vim zsh util-linux-user git dnf-plugins-core yum-utils
+
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+dnf -y install docker-ce
+systemctl enable --now docker
+
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
+mv docker-compose /usr/local/bin && sudo chmod +x /usr/local/bin/docker-compose
+
+# Disable firewalld so container networking works
+systemctl disable firewalld
 
 function git(){
     # Configure github account access 
@@ -42,18 +53,19 @@ function git(){
     mkdir ~/projects
 
 }
+
 function zsh(){
     # This function requries an authenticated github connection to allow download of the .zshrc profile.
     printf "\e[1;32m\nConfiguring zsh and installing oh-my-zsh\e[0m\n\n"
-    
+
     # Change shell for the root user to zsh
-    chsh -s (which zsh) root
+    chsh -s /usr/bin/zsh root
 
     # Download and install oh-my-zh
     wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
     
     # Clone scripts repo containing .zshrc file
-    cd ~/projects 
+    cd ~/projects
     git clone git@github.com:copeia/scripts.git
     cp ~/projects/scripts/bash/vscode-remote/.zshrc ~/.zshrc
     source ~/.zshrc
