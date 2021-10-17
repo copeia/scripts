61c43fc20255aa5ffb37d4662486fac4906ca7e3
Ian Copeland <copeiaj@gmail.com>
2021-10-17 16:11:55 -0700

    Create standard_logging.sh

---
+++ bash/standard_logging.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function log {
+  local -r level="$1"
+  local -r message="$2"
+  local -r timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  >&2 echo -e "${timestamp} [${level}] [$SCRIPT_NAME] ${message}"
+}
+
+function log_info {
+  local -r message="$1"
+  log "INFO" "$message"
+}
+
+function log_warn {
+  local -r message="$1"
+  log "WARN" "$message"
+}
+
+function log_error {
+  local -r message="$1"
+  log "ERROR" "$message"
+}
