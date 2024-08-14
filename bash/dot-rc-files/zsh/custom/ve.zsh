8b99b34cce80a91028aa2830372ba9de99e4a56f
Ian Copeland <ian.j.copeland@citizensbank.com>
2024-08-14 11:38:30 -0600

    feat: Adding ve and vault prompt config

--- bash/dot-rc-files/zsh/custom/ve.zsh
+++ bash/dot-rc-files/zsh/custom/ve.zsh
@@ -1,6 +1,6 @@
 function ve() {
 
-  export VAULT_ADDR="https://vault-enterprise-${1}.corp.internal.citizensbank.com"
+  export VAULT_ADDR="https://vault-enterprise-${1}.corp.internal.<BASE_DOMAIN>.com"
   printf "\ntoken:"
   read -s token
   export VAULT_TOKEN="${token}"
